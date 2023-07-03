7dbf3ba00f5dd6c4ee16d0c74a1113336c0471da
Thilakshan Baskaran <telakshanb@gmail.com>
2023-07-03 16:29:34 -0400

    add classes

---
+++ SM-POST/Post.Cmd/Post.Cmd.Api/Commands/DeletePostCommand.cs
@@ -0,0 +1,6 @@
+namespace Post.Cmd.Api.Commands;
+
+public class DeletePostCommand: BaseCommand
+{   
+    public string Username { get; set; }
+}
---
+++ SM-POST/Post.Common/Events/PostCreatedEvent.cs
@@ -0,0 +1,8 @@
+using CQRS.Core.Events;
+
+namespace Post.Common.Events;
+
+public class PostCreatedEvent: BaseEvent
+{
+
+}
