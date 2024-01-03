ac158fa4555c35dde62b8b8d49de4ef4f189f9de
Thilakshan Baskaran <telakshanb@gmail.com>
2024-01-03 16:03:55 -0500

    docker compose file include mongodb

--- CQRS-ES/CQRS.Core/Events/EventModel.cs
+++ CQRS-ES/CQRS.Core/Events/EventModel.cs
@@ -9,8 +9,8 @@ public class EventModel
     public string Id { get; set; }  
     public DateTime TimeStamp { get; set; }
     public Guid AggregateIdentifier { get; set; }
-    public string AggregateType { get; set; }
+    public string AggregateType { get; set; } = null!;
     public int Version { get; set; }
-    public string EventType { get; set; }
+    public string EventType { get; set; } = null!;
     public BaseEvent EventData { get; set; }
 }
