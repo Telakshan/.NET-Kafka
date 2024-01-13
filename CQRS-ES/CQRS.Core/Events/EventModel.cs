08313e960499af7926ec4211509c326b715d4a3e
Thilakshan Baskaran <telakshanb@gmail.com>
2024-01-12 21:07:20 -0500

    nullable

--- CQRS-ES/CQRS.Core/Events/EventModel.cs
+++ CQRS-ES/CQRS.Core/Events/EventModel.cs
@@ -6,11 +6,11 @@ public class EventModel
 {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
-    public string Id { get; set; }  
+    public string Id { get; set; } = null!;  
     public DateTime TimeStamp { get; set; }
     public Guid AggregateIdentifier { get; set; }
-    public string AggregateType { get; set; }
+    public string? AggregateType { get; set; }
     public int Version { get; set; }
-    public string EventType { get; set; }
-    public BaseEvent EventData { get; set; }
+    public string? EventType { get; set; } 
+    public BaseEvent? EventData { get; set; }
 }
